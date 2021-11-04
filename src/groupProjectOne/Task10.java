d81e8727cd326d3ab97fe59123884e6043e22ff9
Alan <tsdchaos@gmail.com>
2021-11-04 09:10:43 -0400

    Added class 11 notes and half of Group Project

--- src/groupProjectOne/Task10.java
+++ src/groupProjectOne/Task10.java
@@ -1,5 +1,13 @@
 package groupProjectOne;
 
-public class Task10 {
+import java.util.Arrays;
 
+public class Task10 {
+	public static void main(String[] args) {
+		int numbers[] = { 67, 23, 444, -10, 990 };
+		
+		Arrays.sort(numbers);
+		System.out.println("Largest number is " + numbers[numbers.length-1]);
+		System.out.println("Largest number is " + numbers[numbers.length-2]);
+	}
 }
